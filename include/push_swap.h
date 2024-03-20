b1ba37198e000b4149dd9da1a319282533142a80
Bruno Aguiar <aguiarsilva@gmail.com>
2024-03-20 22:11:58 +0100

    updated files

--- include/push_swap.h
+++ include/push_swap.h
@@ -43,18 +43,28 @@ typedef struct s_stack
 int list_len(t_stack *pile);
 t_stack *search_last(t_stack  *pile);
 bool    is_sorted(t_stack *pile);
+t_stack *find_smallest(t_stack   *pile);
+t_stack *find_biggest(t_stack  *pile);
 
 // Commands
 
 static void    swap(t_stack **head);
-void    rra(t_stack **a, bool flag);
+void    sa(t_stack **a, bool flag);
 void    sb(t_stack **b, bool flag);
 void    ss(t_stack **a, t_stack **b, bool flag);
 static void rev_rotate(t_stack **node);
 void    rra(t_stack **a, bool flag);
 void    rrb(t_stack **b, bool flag);
 void    rrr(t_stack **a, t_stack **b, bool flag);
+static void     rotate(t_stack  **node);
+void    ra(t_stack **a, bool flag);
+void    rb(t_stack **b, bool flag);
+void    rr(t_stack **a, t_stack **b, bool flag);
+static void     push(t_stack **dest, t_stack **src);
+void    pa(t_stack **a, t_stack **b, bool flag);
+void    pb(t_stack **a, t_stack **b, bool flag);
 
 // Algorithms used
+void    small_sort(t_stack **a);
 
 #endif
--- src/rotate.c
+++ src/rotate.c
@@ -14,9 +14,41 @@
 
 static void     rotate(t_stack  **node)
 {
-    t_stack *end_node;
+    t_stack *end_node; //create variable with the last node
 
-    if (node == NULL || *node == NULL)
+    if (node == NULL || *node == NULL) //check if stacks exists or the pointer to the node exists
         return;
-    
+    end_node = search_last(*node); //use seach last node function to attribute the last node to the variable
+    //assign the last node to its next attribute as the top node effectively setting the current node to top node as last node
+    end_node->next = *node;
+    //assign to the pointer of the top node, the node after it (second from the top)
+    *node = (*node)->next;
+    //Complete setting the current top node by detaching it from its previous top node
+    (*node)->prev = NULL;
+    //Reconnect the second node's prev pointer to point to what was previously the last node in the stack
+    end_node->next->prev = end_node;
+    //Assign tho the next attribute of the current last node 'NULL' effectively setting it as the current last node and properly null terminating the stack
+    end_node->next->next = NULL;
+}
+
+void    ra(t_stack **a, bool flag)
+{
+    rotate(a);
+    if (!flag)
+        ft_printf("ra\n");
+}
+
+void    rb(t_stack **b, bool flag)
+{
+    rotate(b);
+    if (!flag)
+        ft_printf("rb\n");
+}
+
+void    rr(t_stack **a, t_stack **b, bool flag)
+{
+    rotate(a);
+    rotate(b);
+    if (!flag)
+        ft_printf("rr\n");
 }
